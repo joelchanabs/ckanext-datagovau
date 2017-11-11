2e9171e57e0fa4a84e482eb565f22e21f70b30b7
gvonnessi <greg.vonnessi@linkdigital.com.au>
2017-11-11 14:15:21 +1100

    - Fix for stats error on front page during dataset re-index

--- ckanext/datagovau/helpers.py
+++ ckanext/datagovau/helpers.py
@@ -61,6 +61,9 @@ def get_ddg_site_statistics():
         stats['api_count'] = logic.get_action('resource_search')({}, {"query": ["format:wms"]})['count'] + len(
             datastore_db.get_all_resources_ids_in_datastore())
 
+        if 'unpug_data_count' not in stats:
+            stats['unpub_data_count'] = 0
+
         return stats
 
     if toolkit.asbool(config.get('ckanext.stats.cache_enabled', 'True')):
