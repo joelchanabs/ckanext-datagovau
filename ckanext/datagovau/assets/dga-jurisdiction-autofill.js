df1ea1e1e258f1a2b4cf3df029e7fc3d31f66335
Oleksandr Ivaniuk <oleksandr.ivaniuk@linkdigital.com.au>
2021-12-16 17:11:18 +0200

    ABSXCGHE-35 / dataset jurisdiction field autofill

---
+++ ckanext/datagovau/assets/dga-jurisdiction-autofill.js
@@ -0,0 +1,34 @@
+ckan.module("dga-jurisdiction-autofill", function ($) {
+    "use strict";
+    return {
+        options: {},
+
+        initialize: function () {
+            this.jurisdiction_entered_manually = false;
+            this.new_dataset = ($('.stages')[0]) ? true : false;
+            this.org_field = $('#field-organizations');
+            if (this.new_dataset) {
+                this._onChange(this);
+                this.org_field.on("change", this._onChange.bind(this));
+                this.el.on("input", function () {
+                    this.jurisdiction_entered_manually = true;
+                });
+            }
+        },
+
+        _onChange: async function (e) {
+            console.log(this.org_field.val());
+            if (!this.jurisdiction_entered_manually) {
+                var org_dict = new Promise((ok, fail) => {
+                    fetch(this.sandbox.client.call(
+                        "GET",
+                        "organization_show",
+                        "?id=" + this.org_field.val(),
+                        data => ok(data.result), fail));
+                });
+                const dict = await org_dict;
+                this.el.val(dict['jurisdiction']);
+            }
+        },
+    };
+});
